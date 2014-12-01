d2970403238e8085f9005c81a3ef6231c7a62cd2
Kevin Overall <kmoverall@gmail.com>
2014-12-01 15:28:02 -0500

    Added a sphere object

---
+++ Sphere.cpp
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <array>
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <Windows.h>
+#include <limits>
+#include "Vector3.h"
+#include "Ray.h"
+#include "Solid.h"
+#include "Sphere.h"
+
+Solid::Intercept Sphere::FindRayIntersect(Ray ray) {
+	Solid::Intercept result;
+	result.mat = mat;
+	result.normal = new Vector3();
+	result.point = new Vector3();
+	result.t = std::numeric_limits<float>::infinity();
+
+	//Transform ray to local coordinates
+	ray.inverseTransform(position, rotation, scale);
+	
+	float quadratic = 4 * (ray.direction->dot(ray.start)) * (ray.direction->dot(ray.start))
+		- 4 * ray.direction->mag() * ray.direction->mag() * (ray.start->mag() * ray.start->mag() - 1);
+	if (quadratic >= 0) {
+		float tmp_1 = (-2 * (ray.direction->dot(ray.start)) + sqrtf(quadratic)) / (2 * ray.direction->mag() * ray.direction->mag());
+		float tmp_2 = (-2 * (ray.direction->dot(ray.start)) - sqrtf(quadratic)) / (2 * ray.direction->mag() * ray.direction->mag());
+		if (tmp_1 >= 1E-6 && tmp_2 >= 1E-6) {
+			result.t = min(tmp_1, tmp_2);
+			result.point = ray.start->add(ray.direction->multiply(result.t));
+			result.normal = result.point;
+		}
+		else if (tmp_1 >= 1E-6 || tmp_1 >= 1E-6) {
+			result.t = max(tmp_1, tmp_2);
+			result.point = ray.start->add(ray.direction->multiply(result.t));
+			result.normal = result.point;
+		}
+		int a = 1;
+	}
+
+	//Transforms normals from local to world coords
+	result.point->transform(*position, *rotation, *scale);
+	result.normal->divide(scale);
+	result.normal->normalize();
+
+	return result;
+}
---
+++ Sphere.h
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <array>
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <Windows.h>
+#include <limits>
+#include "Vector3.h"
+#include "Ray.h"
+#include "Material.h"
+#include "Solid.h"
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+class Sphere : public Solid
+{
+public:
+	Vector3* position;
+	Vector3* rotation;
+	Vector3* scale;
+	Solid::Material mat;
+
+	Sphere() {
+		position = new Vector3();
+		rotation = new Vector3();
+		scale = new Vector3(1.0, 1.0, 1.0);
+		mat.color[0] = 255;
+		mat.color[1] = 255;
+		mat.color[2] = 255;
+		mat.alpha = 1.0;
+		mat.shininess = 1.0;
+		mat.specCoeff = 1.0;
+		mat.specExponent = 200.0;
+		mat.reflectionCoeff = 0.0;
+	}
+
+	Intercept FindRayIntersect(Ray ray);
+};
+
+#endif
